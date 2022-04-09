d09fdc04a64943c4379404b25af2edf2e1c4ef59
Angel Yankov <yankov-hw@protonmail.com>
2022-04-09 15:28:55 +0300

    implement v2f struct and overloads in cpp file

--- utils.h
+++ utils.h
@@ -7,20 +7,22 @@
 using namespace std;
 
 typedef struct v2f{
-    v2f();
-    v2f(double x, double y);   
-    bool operator ==(v2f v);
-    v2f operator + (v2f v);
-    v2f operator - (v2f v);
-    double operator *(v2f v);
-    double operator ^(v2f v);
-    double len();
-    v2f getnormalized();
-    void normalize();
-    v2f getortogonal();
-    void orthogonalize();
-    double x, y;
-} v2f;
+	v2f();
+	v2f(double x, double y);
+	double len();
+	void normalize();
+	v2f getnormalized();
+	v2f getortogonal();
+	void orthogonalize();
+	double x, y;
+
+bool operator ==(const v2f& v);
+v2f operator +(const v2f& v);
+v2f operator -(const v2f& v);
+double operator *(const v2f& v);
+double operator ^(const v2f& v);
+} v2f_st;
+
 
 double cos(v2f v1, v2f v2);
 double sin(v2f v1, v2f v2);
@@ -30,15 +32,14 @@ struct edge {
     edge(v2f a, v2f b);
     v2f a, b;
     double len;
-    bool operator == (edge e);
     void edit(v2f a, v2f b);
+    bool operator ==(const edge& e);
 };
 
 struct triangle {
     triangle();
     triangle(v2f A, v2f B, v2f C);
     bool IsPointInCircumCircle(v2f P);
-    bool operator == (triangle t);
     double GetArea();
     v2f CircumCenter;
     vector<v2f> points;
@@ -46,6 +47,8 @@ struct triangle {
     vector<edge> edges;
     double CircumRadius, alpha, beta, gamma;
     bool IsBad;
+	bool operator== (const triangle& rt);
+
 };
 
 #endif
